e9fe41fc801527b16f1fc631fa6eed7b04739821
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2025-04-01 12:21:03 -1000

    small update to documentation

--- code/00-html-viewer.r
+++ code/00-html-viewer.r
@@ -27,25 +27,26 @@
 # define OS
     linux_ws = is_linux_os() # flag to switch code depending on which workspace is being used
 #________________________________________________________________________________________________________________________________________________________________________________________________________
-# show available directories
-    all_dirs = list.files(model_stem,recursive = TRUE)
-    all_dirs = all_dirs[grep("summary.csv",all_dirs,fixed=TRUE)]
-    all_dirs = all_dirs[-grep("fleet_summary.csv",all_dirs,fixed=TRUE)]
-    all_dirs = gsub("summary.csv","",all_dirs,fixed=TRUE)
-    print(all_dirs)
-    target_dir = file.path(model_stem,all_dirs)[1] # specify which model you want to make the html for
+# Uncomment the below code if you intend to specifically run ss_model_html_viewer on a particular directory and clean up the files afterwards    
+    # # show available directories
+    # all_dirs = list.files(model_stem,recursive = TRUE)
+    # all_dirs = all_dirs[grep("summary.csv",all_dirs,fixed=TRUE)]
+    # all_dirs = all_dirs[-grep("fleet_summary.csv",all_dirs,fixed=TRUE)]
+    # all_dirs = gsub("summary.csv","",all_dirs,fixed=TRUE)
+    # print(all_dirs)
+    # target_dir = file.path(model_stem,all_dirs)[1] # specify which model you want to make the html for
     
-    # make html & plots for desired model run
-    ss_model_html_viewer(target_dir,html_dir=html_location,linux_ws=linux_ws, catchasnumbers = TRUE)
+    # # make html & plots for desired model run
+    # ss_model_html_viewer(target_dir,html_dir=html_location,linux_ws=linux_ws, catchasnumbers = TRUE)
 
-    # if viewing on GitHub Codespaces, navigate to the file.
-    # Right-click and select preview. This will open the file in a window within Codespaces.
+    # # if viewing on GitHub Codespaces, navigate to the file.
+    # # Right-click and select preview. This will open the file in a window within Codespaces.
 
-    # clean-up since these plots take up a lot of space
-    clean_files = c(list.files(html_location),paste0("plots/",list.files(file.path(html_location,"plots"))))
-    print(paste0(length(clean_files)," files removed taking up ",round(sum(unname(sapply(file.path(html_location,clean_files),file.size)))/1024^2,digits=2)," MB."))
-    unname(sapply(file.path(html_location,clean_files),unlink,recursive=TRUE))
-    print(paste0("Directory size is now ",round(sum(unname(sapply(file.path(html_location,list.files(html_location)),file.size)))/1024^2,digits=2)," MB."))
+    # # clean-up since these plots take up a lot of space
+    # clean_files = c(list.files(html_location),paste0("plots/",list.files(file.path(html_location,"plots"))))
+    # print(paste0(length(clean_files)," files removed taking up ",round(sum(unname(sapply(file.path(html_location,clean_files),file.size)))/1024^2,digits=2)," MB."))
+    # unname(sapply(file.path(html_location,clean_files),unlink,recursive=TRUE))
+    # print(paste0("Directory size is now ",round(sum(unname(sapply(file.path(html_location,list.files(html_location)),file.size)))/1024^2,digits=2)," MB."))
 
 
 
